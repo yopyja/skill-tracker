93a5df398c75b10dc10edb113b2c72972ff3906c
DiracsCoup <110425105+TernaryGriffin@users.noreply.github.com>
2022-09-13 10:54:50 -0600

    major gains RAWWWWWWWW

--- skill_tracker/models.py
+++ skill_tracker/models.py
@@ -1,3 +1,81 @@
+from pydoc import describe
+from pyexpat import model
 from django.db import models
 
 # Create your models here.
+class Departments(models.Model):
+    testing = models.AutoField
+
+class Team(models.Model):
+    team_id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=50)
+    description = models.TextField(null=True)
+    is_active = models.BooleanField()
+    logo = models.BinaryField(null=True)
+
+class Organization(models.Model):
+    org_id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=50)
+    logo = models.BinaryField(null=True)
+
+class Roles(models.Model):
+    role_id = models.AutoField(primary_key=True)
+    org_id = models.ForeignKey(Organization.org_id)
+    is_team = models.BooleanField()
+
+class Sub_Permissions(models.Model):
+    sub_permission_id = models.AutoField(primary_key=True)
+    description = models.CharField(max_length=300)
+
+class Granted_Permissions(models.Model):
+    granted_permissions_id = models.AutoField(primary_key=True)
+    is_user = models.BooleanField()
+    sub_permissions_id = models.ForeignKey(Sub_Permissions.sub_permissions_id)
+    entity_id = models.IntegerField()
+
+class User(models.Model):
+    user_id = models.AutoField(primary_key=True)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    prefix = models.CharField(max_length=5)
+    position = models.CharField(max_length=50)
+
+class Skill_Level(models.Model):
+    skill_level_id = models.AutoField(primary_key=True)
+    rating_type = models.IntegerField
+    rating_value = models.CharField(max_length=50)
+
+class Skill(models.Model):
+    skill_id = models.AutoField(primary_key=True)
+    skill_label = models.CharField(max_length=75)
+    
+class Skill_Group(models.Model):
+    skill_group_id = models.AutoField(primary_key=True)
+    group_label = models.CharField(max_length=75)
+    
+class Sub_Rating_Types(models.Model):
+    rating_type_id = models.AutoField(primary_key=True)
+    description = models.CharField(max_length=50)
+
+class Logging(models.Model):
+    logging_id = models.AutoField(primary_key=True)
+    user_id = models.ForeignKey(User.user_id, null=True)
+    skill_id = models.ForeignKey(Skill.skill_id, null=True)
+    in_training = models.BooleanField(null=True)
+    date = models.DateTimeField
+    action = models.CharField(max_length=75)
+    
+class Assoc_User_Skills(models.Model):
+    user_skill_id = models.AutoField(primary_key=True)
+    user_id = models.ForeignKey(User.user_id)
+    skill_level_id = models.ForeignKey(Skill_Level)
+
+class Assoc_User_Teams(models.Model):
+    team_user_id = models.AutoField(primary_key=True)
+    user_id = models.ForeignKey(User.user_id)
+    team_id = models.ForeignKey(Team.team_id)
+    
+class Assoc_Group_Skills(models.Model):
+    group_skill_id = models.AutoField(primary_key=True)
+    skill_group_id = models.ForeignKey(Skill_Group.skill_group_id)
+    skill_id = models.ForeignKey(Skill.skill_id)
--- skill_tracker/urls.py
+++ skill_tracker/urls.py
@@ -13,18 +13,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path, include
-# import skill_tracker.views
-# import yeet.views
 
-
-
+from django.urls import path
+from skill_tracker import views
 
 urlpatterns = [
-    path('', include('api.urls')),
-    path('api/', include('api.urls')),
-    path('skill_tracker/', include('skill_tracker.urls')),
-    # path('', api.views.index, name='index'),
-    path('admin/', admin.site.urls),
+    path('', views.index, name='index')
 ]
