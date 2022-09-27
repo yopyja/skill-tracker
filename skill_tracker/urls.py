43de77e0e74af9eec799346cfa730b1babe08664
TernaryGriffin <110425105+TernaryGriffin@users.noreply.github.com>
2022-09-27 12:01:57 -0600

    more

--- skill_tracker/urls.py
+++ skill_tracker/urls.py
@@ -27,36 +27,5 @@ urlpatterns = [
     path("dashboard/", views.dashboard, name="dashboard"),
 
     path("edit_user/", views.edit_user, name="edit_user"),
-
-    path('api/', views.TeamApi), 
-    
-    path('api/<int:id>', views.TeamApi),
-
-    path('api/<int:id>', views.OrganizationApi),
-
-    path('api/<int:id>', views.RolesApi),
-    
-    path('api/<int:id>', views.Sub_PermissionsApi),
-
-    path('api/<int:id>', views.Granted_PermissionsApi),
-
-    path('api/<int:id>', views.UserApi),
-
-    path('api/<int:id>', views.Skill_LevelApi),
-
-    path('api/<int:id>', views.SkillApi),
-
-    path('api/<int:id>', views.Skill_GroupApi),
-
-    path('api/<int:id>', views.Sub_Rating_TypesApi),
-
-    path('team/<int:id>', views.LoggingApi),
-
-    path('team/<int:id>', views.Assoc_User_SkillsApi),
-
-    path('team/<int:id>', views.Assoc_User_TeamsApi),
-
-    path('team/<int:id>', views.Assoc_User_SkillsApi),
-
     
 ]
